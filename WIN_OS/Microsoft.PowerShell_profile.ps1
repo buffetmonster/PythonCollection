86740f19505602fa271eb546e1bf67e7f0c2cb1b
Dave Swan <Dave.Swan@bfbs.com>
2025-03-03 09:47:44 +0000

    update powerscript shell ps1 to set full path as default path got overwritten, some python paths need to be added also

--- WIN_OS/Microsoft.PowerShell_profile.ps1
+++ WIN_OS/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,6 @@
 #The windows powershell console has a profile ps1 file, to find location: $profile
 #for example this might be: C:\Users\theuser\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+#Using this alias we can easily enable the python virtualenv assuming it is setup
 #by editing Microsoft.PowerShell_profile.ps1 we can add some alises related to the powershell
 #some weirdo restriction prevents creating the powershell directory using console, use file explorer to create it
 #below useful if restriction not present
@@ -16,6 +17,18 @@ New-Alias go_virtualenv .\venv\Scripts\activate
 function go_pythonrepo {
     cd \GitRepos\PythonCollection\WIN_OS
 }
+#setup enviromental stuff:
+# For User variables (current user):
+# Modify the User-level PATH (no admin rights needed):
+$newPath = "C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;C:\WINDOWS\System32\OpenSSH\;C:\Program Files\dotnet\;C:\Program Files\PuTTY\;C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;C:\WINDOWS\System32\OpenSSH\;C:\Program Files\dotnet\;C:\Program Files\PuTTY\;C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;C:\WINDOWS\System32\OpenSSH\;C:\Program Files\dotnet\;C:\Program Files\PuTTY\;C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;C:\WINDOWS\System32\OpenSSH\;C:\Program Files\dotnet\;C:\Program Files\PuTTY\;C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;C:\WINDOWS\System32\OpenSSH\;C:\Program Files\dotnet\;C:\Program Files\PuTTY\;C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;C:\WINDOWS\System32\OpenSSH\;C:\Program Files\dotnet\;C:\Program Files\PuTTY\;C:\Users\Dave.Swan\AppData\Local\Microsoft\WindowsApps;C:\Users\Dave.Swan\AppData\Local\Programs\Microsoft VS Code\bin;C:\Users\Dave.Swan\AppData\Local\Programs\Git\cmd\;C:\Users\Dave.Swan\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.13_qbz5n2kfra8p0\;C:\Users\Dave.Swan\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.13_qbz5n2kfra8p0\LocalCache\local-packages\Python313\Scripts\;"
+
+[Environment]::SetEnvironmentVariable("Path", $newPath, "User")
+
+#for poetry:
+#C:\Users\Dave.Swan\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.13_qbz5n2kfra8p0\LocalCache\local-packages\Python313\Scripts
+#C:\Users\Dave.Swan\AppData\Local\Programs\Git\cmd;C:\GitRepos\PythonCollection\WIN_OS\qt6_gui\venv\Scripts\
+
+$env:Path
 ECHO "Added Alias: go_virtualenv"
 ECHO "Added Alias: go_createenv"
 ECHO "Added Alias: go_pythonrepo"
